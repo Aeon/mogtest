7f0bedab243d2f211c77bfd4bbe96c98f7a4ff51
Anton Stroganov <stroganov.a@gmail.com>
2010-07-05 23:31:49 -0700

    meh

--- slashdot.rb
+++ slashdot.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/env ruby
-
+# require 'rubygems'
+# require 'ruby-debug'
 # let's just grab the items info, assume we don't care about the rest of the feed metadata.
+
+grammar = {
+  :document => ':xml_head :tree',
+  :xml_head => /^<?xml .*>$/,
+  :tree => ':tag :branch :close_tag | :self_tag',
+  :branch => ':tree | :value',
+  :self_tag => '<:word( :word=":value")* />',
+  :tag => '<:word( :word=":value")*>',
+  :close_tag => '</:word>',
+  :word => /[^\s<>]*/,
+  :value => /[^<>]*/
+}
+# 
+# def parse(buffer) do
+#   until buffer.empty? do
+#     grammar.each do |name, pattern|
+#       pattern.match()
+#     end
+#   end
+# end
 
 def get_items(filename)
   reading_item = false
@@ -27,10 +48,91 @@ def get_items(filename)
   feed_items
 end
 
-# only gets the first tag anyway.
-def get_tag_content(input, tagname)
-  rx = Regexp.new("<#{tagname}[^>]*>([^<>]*)</#{tagname}>")
-  content = input.match(rx)
+tree = {}
+$open_tag_rx = Regexp.new("<([^\s>]*)([^>]*)?>")
+
+def parse_tree(buffer, tree)
+  until buffer.empty? do
+    match = $open_tag_rx.match(buffer)
+    tag = {}
+    if match
+      tag[:tag] = match[1]
+      buffer = match.post_match
+      close_match = Regexp.new("</#{tag[:tag]}>").match(buffer)
+      tag[:text] = close_match.pre_match
+      buffer = match.post_match
+      return tag
+    else
+    end
+  end
 end
+
+def build_tree(buffer)
+  tree = []
+  until buffer.empty?
+    next_tag = get_next_tag(buffer)
+    if next_tag.nil?
+      tree[] << buffer
+    else
+      tree[] << next_tag
+    end
+  end
+end
+
+def get_next_tag(buffer)
+  match = $open_tag_rx.match(buffer)
+  tag = {
+    :tag => nil,
+    :text => nil,
+    :attributes => nil,
+    :children => nil
+  }
+  if match
+    tag[:tag] = match[1]
+    unless match[2].nil?
+      tag[:attributes] = parse_attributes(match[2])
+    end
+    buffer = match.post_match
+    close_match = Regexp.new("</#{tag[:tag]}>").match(buffer)
+    content = close_match.pre_match
+    tag[:text] = content
+    buffer = match.post_match
+    return tag
+  else
+    return buffer
+  end
+end
+
+def parse_attributes(string)
+  attributes = {}
+  until string.empty?
+    match = /\s*([^=]*)="([^"]*)"\s*/.match(string)
+    if match
+      attributes[match[1].intern] = match[2]
+    end
+    string = match.post_match
+  end
+  attributes
+end
+
+def build_tree(buffer, tree)
+  # find opening tag in buffer
+  tag = buffer.match($open_tag_rx)
+  unless tag.nil?
+    return {:tag => tag, :children => build_tree(content, tree)}
+  else
+    return {:content => buffer}
+  end
+  # if tag has been found
+    # parse tag attributes
+    # return tag
+  # else
+    # return the buffer
+  # stick everything after it into buffer
+  # find closing tag
+  # run build_triee on buffer
+end
+
+# tree = build_tree(buffer, tree)
 
 # puts get_items('slashdot.rss')[2].inspect
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -12,4 +12,36 @@ class ParserTest < Test::Unit::TestCase
     item_count = get_items('slashdot.rss').size
     assert(item_count == 15, "Got #{item_count} items instead of 15")
   end
+  
+  def test_get_tag
+    str = '<item>Description</item>'
+    tag = get_next_tag(str)
+    assert(tag[:tag] == 'item', "tag name is #{tag[:tag]}")
+    assert(tag[:text] == 'Description', "tag text is incorrect #{tag[:text]}")
+  end
+
+  def test_parse_attributes
+    str = ' link="http://foobar.com" bar="baz"'
+    attributes = parse_attributes(str)
+    assert(!attributes.empty?, "no attributes found")
+    assert(attributes[:link] == 'http://foobar.com', "link attribute is #{attributes[:link]}")
+    assert(attributes[:bar] == 'baz', "bar attribute is #{attributes[:bar]}")
+  end
+
+  def test_get_tag_with_attributes
+    str = '<item link="http://foobar.com">Description</item>'
+    tag = get_next_tag(str)
+    assert(tag[:tag] == 'item', "tag name is #{tag[:tag]}")
+    assert(!tag[:attributes].nil?, "tag attributes are missing")
+    assert(tag[:attributes][:link] == 'http://foobar.com', "tag link attribute is incorrect #{tag[:attributes][:link]}")
+  end
+  
+  # def test_parse_simple_tree
+  #   str = '<item>Description</item>'
+  #   tree = parse_tree(str, {})
+  #   assert(tree[:tag] == 'item', 'tag name is incorrect')
+  #   assert(tree[:text] == 'Description', 'tag text is incorrect')
+  #   assert(tree[:children].empty?, 'tag children are not empty')
+  #   assert(tree[:attributes].empty?, 'tag attributes are not empty')
+  # end
 end
